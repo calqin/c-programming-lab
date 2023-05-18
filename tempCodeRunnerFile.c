3395416fd9247d668b169e1ddb4ccc6e701e0630
Arjun Suwedi <117531393+calqin@users.noreply.github.com>
2023-05-18 22:52:13 +0530

    Add files via upload

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,11 +1,1 @@
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-    for (int i=1;i<=5;i++){
-        for (int j=0 ;j<i ;j++){
-            printf("%d",(i+j));
-        }
-        printf("\n");
-    }
-    return 0;
-}
+b
